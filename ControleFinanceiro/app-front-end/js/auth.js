7fda40c86c75da68bdea3c5f55c6f209019c90a2
EricPinto99 <ericgrandopinto@gmail.com>
2019-12-12 10:21:14 -0300

    alterados arquivos de autenticação

--- ControleFinanceiro/app-front-end/js/auth.js
+++ ControleFinanceiro/app-front-end/js/auth.js
@@ -4,14 +4,13 @@ document.querySelector("#buscarTokenBtn").addEventListener("click", e => buscarT
 
 function buscarToken(e) {
     e.preventDefault();
-    // busca os valores digitados nos campos de usuário e senha
+    
     var login = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // verifica se os campos tem conteúdo 
+    
     if(login != "" && password != "") {
         
-        // cria uma estrutura JSON para os dados obtidos do front-end
         dados = {
             login: login,
             password: password
@@ -26,14 +25,14 @@ function buscarToken(e) {
 
         req.done(res => {
             console.log('done',res)
-            //res.JSON.parse(this.responseText);
+
             sessionStorage.setItem('token',res.token);
             window.location.href = "main.html";
-        }) /* sucesso */
+        }) 
         
         req.fail(res => {
             document.querySelector("#mensagem").innerHTML = "Login ou senha inválidos!"; 
-        }) /* erro */
+        }) 
     
     } else {
         document.querySelector("#mensagem").innerHTML = "Por favor, preencha todos os campos";        
